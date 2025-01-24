3ebaff7f583fadf72582cd91f6b4b6fa46926678
Chepkosgei Bii <cbii@goldengate.cs.brynmawr.edu>
2025-01-24 04:09:10 -0500

    Descriptive message

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Chepkosgei Bii
  * Implements a math game
- */
-
-#include <stdio.h>
+ **************************************************/
 
 int main() {
-  return 0;
+    int rounds, correct = 0;
+
+    srand(time(NULL)); 
+
+    printf("Welcome to Math Game!\n");
+    printf("How many rounds do you want to play? ");
+    scanf("%d", &rounds);
+
+    for (int i = 0; i < rounds; i++) {
+        int a = rand() % 9 + 1; 
+        int b = rand() % 9 + 1; 
+        int answer;
+
+        printf("%d + %d = ? ", a, b);
+        scanf("%d", &answer);
+
+        if (answer == a + b) {
+            printf("Correct!\n");
+            correct++;
+        } else {
+            printf("Incorrect :( \n");
+        }
+    }
+
+    printf("You answered %d/%d correctly.\n", correct, rounds);
+    return 0;
 }
